07dc515e042461a325a36425e8eb32f48851b6af
davidli <chuntaoli@hotmail.com>
2023-06-17 15:59:25 -0400

    added Pets and Contact info to owners on startup. Close #28

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -3,10 +3,19 @@ package guru.springframework.sfgpetclinic.model;
 import java.time.LocalDate;
 
 public class Pet extends BaseEntity{
+    private String Name;
     private PetType petType;
     private Owner owner;
 
     private LocalDate birthDate;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
 
     public PetType getPetType() {
         return petType;
