103fd3256ee48a43ff535d5d21f23d4afa497db3
davidli <chuntaoli@hotmail.com>
2023-07-24 23:40:39 -0400

    updated visit with JPA . Close #33

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -1,8 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
+import javax.persistence.*;
 import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity{
+    @Column(name = "date")
     private LocalDate date;
+    @Column (name = "description")
     private String description;
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
+    private Pet pet;
+
 
     public LocalDate getDate() {
         return date;
@@ -28,6 +39,6 @@ public class Visit extends BaseEntity{
         this.pet = pet;
     }
 
-    private Pet pet;
+
 
 }
